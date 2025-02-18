68fa3412a384d214cae288ab9d8e17c34c1495c3
Janitha S. Karunarathna <janithakarunarathna12@gmail.com>
2025-02-18 21:10:14 +0530

    some minor confifgurations

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://quizcrash.senani.online"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true
     }
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 4001;
 
 // CORS configuration
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://quizcrash.senani.online"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 };
