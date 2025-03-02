e64b4cdd6e9ba4c68626b2e7d25f55beb157f838
Janitha Senani Karunarathna <janithakarunarathna12@gmail.com>
2025-03-02 12:05:20 +0530

    change time to 30 seconds

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ io.on("connection", (socket) => {
 
             room.questionTimer = setTimeout(() => {
                 handleQuestionTimeout(roomId, io);
-            }, 15000);
+            }, 30000);
 
             io.to(roomId).emit("question", {
                 question: {
@@ -117,7 +117,7 @@ io.on("connection", (socket) => {
                     options: questions[0].options,
                     questionNumber: 1,
                     totalQuestions: questions.length,
-                    timeLimit: 15
+                    timeLimit: 30
                 }
             });
         }
@@ -156,7 +156,7 @@ io.on("connection", (socket) => {
             io.to(roomId).emit("quiz-status-update", {
                 totalParticipants: room.participants.length,
                 answeredCount: room.answeredParticipants.size,
-                timeRemaining: Math.max(0, 15 - timeTaken)
+                timeRemaining: Math.max(0, 30 - timeTaken)
             });
 
             socket.emit("answer-result", { isCorrect, score, timeTaken });
@@ -246,7 +246,7 @@ function moveToNextQuestion(roomId, io) {
         // Set a new timer for the next question
         room.questionTimer = setTimeout(() => {
             handleQuestionTimeout(roomId, io);
-        }, 15000);
+        }, 30000);
 
         // Emit the next question to all participants
         const currentQuestion = room.questions[room.currentQuestionIndex];
@@ -256,7 +256,7 @@ function moveToNextQuestion(roomId, io) {
                 options: currentQuestion.options,
                 questionNumber: room.currentQuestionIndex + 1,
                 totalQuestions: room.questions.length,
-                timeLimit: 15
+                timeLimit: 30
             }
         });
 
@@ -264,7 +264,7 @@ function moveToNextQuestion(roomId, io) {
         io.to(roomId).emit("quiz-status-update", {
             totalParticipants: room.participants.length,
             answeredCount: 0, // Reset answered count
-            timeRemaining: 15 // Reset time remaining
+            timeRemaining: 30 // Reset time remaining
         });
     }
 }
